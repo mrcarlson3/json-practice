e0b3d5920b8946d06ea7a77dcde8ba86c15355d1
Michael Carlson <mcarlsonr5@gmail.com>
2024-11-20 16:29:26 -0500

    start py file

---
+++ labs/lab8/parse.py
@@ -0,0 +1,23 @@
+#!/opt/homebrew/bin/python3.13
+
+import json
+import csv
+
+
+with open('../data/schacon.repos.json', 'r') as file:
+    data = json.load(file)
+
+with open('chacon.csv', "w", newline="") as csvfile:
+    csv_writer = csv.writer(csvfile)
+
+    csv_writer.writerow(["name", "html_url", "updated_at", "visibility"])
+    
+    for i, item in enumerate(data[:5]):  
+        csv_writer.writerow([
+            item.get("name", ""),
+            item.get("html_url", ""),
+            item.get("updated_at", ""),
+            item.get("visibility", "")
+        ])
+
+file.close()
